109f788d65c2e60fce6cbade28307a33192f725d
kcbighuge <kcbighuge@gmail.com>
2017-04-02 01:20:44 -0700

    randomly drop "0 angle" imgs from training, 5 epochs

--- drive.py
+++ drive.py
@@ -45,7 +45,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 16  # start with 9
 controller.set_desired(set_speed)
 
 
@@ -62,7 +62,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)  # convert colorspace
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2HSV)  # convert colorspace
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
